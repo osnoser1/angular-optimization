52ea47bb20b42a624218aca18c9db48cc008e5f4
Alfonso Andrés López Molina <alfonso.lopez@merlinjobs.com>
2019-09-10 13:46:43 -0500

    feat: pure pipes and memoization

--- src/app/core/pipes/number-to-string.pipe.ts
+++ src/app/core/pipes/number-to-string.pipe.ts
@@ -1,17 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+
+import { memoize } from 'lodash-es';
+
 import { SpanishNumberConverterService } from '../services/spanish-number-converter.service';
 
 @Pipe({
   name: 'numberToString',
-  pure: false,
 })
 export class NumberToStringPipe implements PipeTransform {
   public constructor(
     private numberConverterService: SpanishNumberConverterService,
   ) {}
 
-  transform(value: number): string {
+  transform = memoize((value: number): string => {
     console.log('transform pipe');
     return this.numberConverterService.toString(value);
-  }
+  });
 }
